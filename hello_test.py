f2f1aca8fd5cec02206947e3efddde3fc62f56fd
Alfredo Garbuno Iñigo <alfredo.garbuno@itam.mx>
2022-07-21 16:43:32 -0500

    Update hello_test.py

--- hello_test.py
+++ hello_test.py
@@ -1,4 +1,7 @@
 import hello;
 
 def test_hello():
-    assert hello.hello_world() == "Hello World!"
+    assert hello.hello_world("World") == "Hello World!"
+
+def test_print():   
+    assert hello.hello_world("Alfredo") == "Hello Alfredo!"
